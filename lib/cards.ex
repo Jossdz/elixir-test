22718527476ee94b9083c28903d06cf088201371
Joss <carloscorrea.dev@gmail.com>
2020-02-20 19:22:50 -0600

    case + pipe + deps

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,6 +1,13 @@
 defmodule Cards do
   def create_deck do
-    ["Ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+
+    # List.flatten(cards)
   end
 
   def shuffle(deck) do
@@ -10,5 +17,29 @@ defmodule Cards do
   # ? tells the function will return a boolean
   def contains?(deck, hand) do
     Enum.member?(deck, hand)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    # {status, binary} = File.read(filename)
+
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "The file does not exist"
+    end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck()
+    |> Cards.shuffle()
+    |> Cards.deal(hand_size)
   end
 end
